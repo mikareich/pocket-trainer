ed8ee3e6faa593443cc503fc36444d799c71852c
Mika Reich <mikareich0@gmail.com>
2021-09-04 21:04:29 +0200

    export all

---
+++ utils/emotion/index.ts
@@ -0,0 +1,6 @@
+export * from "./colors";
+export { default as colors } from "./colors";
+export { default as typescale } from "./typescale";
+export * from "./themes";
+export { default as GlobalStyle } from "./GlobalStyle";
+export * from "./interfaces";
