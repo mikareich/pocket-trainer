6f93d71a05dde80d36583198df9670d5d996d5cc
Mika Reich <mikareich0@gmail.com>
2021-09-04 21:02:54 +0200

    create global style

---
+++ utils/emotion/GlobalStyle.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useTheme, Global, css } from "@emotion/react";
+
+function GlobalStyle() {
+  const theme = useTheme();
+
+  return (
+    <Global
+      styles={css`
+        * {
+          box-sizing: border-box;
+        }
+
+        body {
+          margin: 0;
+          min-height: 100vh;
+          background-color: ${theme.bgColor};
+          font-family: ${theme.fontFamily};
+        }
+      `}
+    />
+  );
+}
+
+export default GlobalStyle;
