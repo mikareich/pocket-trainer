b9f1245fa282c3a01544637826830d976885e16a
Mika Reich <mikareich0@gmail.com>
2021-09-04 21:01:48 +0200

    add typescale

---
+++ utils/emotion/typescale.ts
@@ -0,0 +1,12 @@
+const typescale = {
+  heading1: "49px",
+  heading2: "39px",
+  heading3: "31px",
+  heading4: "25px",
+  heading5: "20px",
+  text: "16px",
+  small: "12px",
+  link: "16px",
+};
+
+export default typescale;
