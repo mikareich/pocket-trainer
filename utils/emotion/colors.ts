fb4c36b38d74caef08231a88bf7d991c5454db37
Mika Reich <mikareich0@gmail.com>
2021-09-04 20:10:04 +0200

    create colors

---
+++ utils/emotion/colors.ts
@@ -0,0 +1,51 @@
+export const gray = {
+  grey100: "#ECEFF4",
+  gray200: "#E5E9F0",
+  gray300: "#D8DEE9",
+  gray400: "#4C566A",
+  gray500: "#434C5E",
+  gray600: "#3B4252",
+  gray700: "#2E3440",
+};
+
+export const blue = {
+  blue100: "#8FBCBB",
+  blue200: "#88C0D0",
+  blue300: "#81A1C1",
+  blue400: "#5C6BC0",
+};
+
+export const red = {
+  red100: "#BF616A",
+};
+
+export const orange = {
+  orange100: "#D08770",
+};
+
+export const yellow = {
+  yellow100: "#EBCB8B",
+};
+
+export const green = {
+  green100: "#A3BE8C",
+};
+
+export const purple = {
+  purlple100: "#B48EAD",
+};
+
+export const black = gray.gray700;
+export const white = gray.grey100;
+
+const colors = {
+  ...gray,
+  ...blue,
+  ...red,
+  ...orange,
+  ...yellow,
+  ...green,
+  ...purple,
+};
+
+export default colors;
